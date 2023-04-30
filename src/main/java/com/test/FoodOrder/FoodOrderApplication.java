9c36884a5f06676f7a2628c4de904cd80bcd98df
vijaychopadexp <132186736+vijaychopadexp@users.noreply.github.com>
2023-04-30 12:01:59 +0530

    Update FoodOrderApplication.java

--- src/main/java/com/test/FoodOrder/FoodOrderApplication.java
+++ src/main/java/com/test/FoodOrder/FoodOrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodOrderApplication {
 
 	public static void main(String[] args) {
+		//start foodn service
 		SpringApplication.run(FoodOrderApplication.class, args);
 	}
 
