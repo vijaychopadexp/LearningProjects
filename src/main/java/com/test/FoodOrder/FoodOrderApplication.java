6972fb43f3517b4b54cf570478f4799864ae064c
vijaychopadexp <meetvjchopade@gmail.com>
2023-04-30 12:05:53 +0530

    updated

--- src/main/java/com/test/FoodOrder/FoodOrderApplication.java
+++ src/main/java/com/test/FoodOrder/FoodOrderApplication.java
@@ -3,6 +3,7 @@ package com.test.FoodOrder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test start
 @SpringBootApplication
 public class FoodOrderApplication {
 
